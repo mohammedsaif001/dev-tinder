c1899f75ab4102e96009fa3bc85f67e756893ad6
Mohammed Saif <mohammed001saif@gmail.com>
2025-08-31 20:22:25 +0530

    Encrypting Password

--- src/app.js
+++ src/app.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const bcrypt = require("bcrypt");
+const { validateSignUpData } = require("./utils/validation");
 const app = express();
 const User = require("./models/user");
 
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  //   Creating a new instance of the User model
-  const user = new User(req.body);
-
   try {
+    // Validation of data
+    validateSignUpData(req);
+
+    const { firstName, lastName, emailId, password } = req.body;
+
+    // Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    //   Creating a new instance of the User model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
     await user.save();
     res.send("User Added successfully!");
   } catch (err) {
