8390da4cc4d55690499efe8224f5f280c1bcc3f4
Mohammed Saif <mohammed001saif@gmail.com>
2025-09-13 12:07:00 +0530

    Edit USer Profile API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middleware/auth");
+const { validateEditProfileData } = require("../utils/validations");
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+// API To Fetch User Profile
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+// API To Edit User Profile
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Edit Request");
+    }
+
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+
+    await loggedInUser.save();
+
+    res.json({
+      message: `${loggedInUser.firstName}, your profile updated successfully`,
+      data: loggedInUser,
+    });
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
