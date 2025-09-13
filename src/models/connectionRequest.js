d9c8bc60b085e3e3ad0236907575cd171b92b441
Mohammed Saif <mohammed001saif@gmail.com>
2025-09-13 12:17:13 +0530

    Adding a connection Request model

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: `{VALUE} is incorrect status type`,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+// This will ensure the query is faster when doing lookup (Indexing)
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
+// This will execute before saving a connection request
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this;
+  // Check if the fromUserId is same as toUserId
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("Cannot send connection request to yourself!");
+  }
+  next();
+});
+
+const ConnectionRequestModel = new mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
+
+module.exports = ConnectionRequestModel;
